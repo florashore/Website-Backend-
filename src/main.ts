8903b9f0576b32d548bfbbc570bb06c1cb747dfd
small <small.crypto.coin@gmail.com>
2025-09-26 09:09:44 +0200

    feat(swagger): implement api test using swagger

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +12,16 @@ async function bootstrap() {
     forbidNonWhitelisted: true,
     transform: true,
   }));
+  
+  // Swagger setup
+  const config = new DocumentBuilder()
+    .setTitle('Portfolio API')
+    .setDescription('API documentation')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
   
   await app.listen(process.env.PORT ?? 8080);
 }
